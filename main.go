f0a12120882aafaee0ce83caf5809bac1bcbc70a
wtrindades <wtrindades@gmail.com>
2022-01-12 19:39:21 -0300

    feat: add CheckingAccount#withdraw

--- main.go
+++ main.go
@@ -9,8 +9,26 @@ type CheckingAccount struct {
 	balance       float64
 }
 
+func (c *CheckingAccount) withdraw(value float64) string {
+	if value >= 0 && value <= c.balance {
+		c.balance -= value
+		return "Successful withdrawal"
+	} else {
+		return "Insufficient funds"
+	}
+}
+
 func main() {
 	myAccount := CheckingAccount{owner: "Wagner", agency: 123, accountNumber: 123456, balance: 1000.00}
 
 	fmt.Println(myAccount)
+
+	fmt.Println(myAccount.withdraw(200))
+	fmt.Println(myAccount.balance)
+
+	fmt.Println(myAccount.withdraw(1200))
+	fmt.Println(myAccount.balance)
+
+	fmt.Println(myAccount.withdraw(-200))
+	fmt.Println(myAccount.balance)
 }
