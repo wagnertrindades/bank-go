1c85b04262c50f8336aed1c5da45afc1735a5d91
wtrindades <wtrindades@gmail.com>
2022-01-12 21:02:57 -0300

    refactor: extract CheckingAccount struct to package

---
+++ accounts/checkingAccount.go
@@ -0,0 +1,37 @@
+package accounts
+
+type CheckingAccount struct {
+	Owner         string
+	Agency        int
+	AccountNumber int
+	Balance       float64
+}
+
+func (c *CheckingAccount) Withdraw(value float64) (string, bool) {
+	if value > 0 && value <= c.Balance {
+		c.Balance -= value
+		return "Successful withdrawal", true
+	}
+
+	return "Insufficient funds", false
+}
+
+func (c *CheckingAccount) Deposit(value float64) (string, float64) {
+	if value > 0 {
+		c.Balance += value
+		return "Successful deposited", c.Balance
+	}
+
+	return "The value should be higher than zero", c.Balance
+}
+
+func (c *CheckingAccount) Transfer(value float64, destinyAccount *CheckingAccount) string {
+	_, withdrawed := c.Withdraw(value)
+
+	if withdrawed {
+		destinyAccount.Deposit(value)
+		return "Successful transfer"
+	}
+
+	return "Insufficient funds"
+}
--- main.go
+++ main.go
@@ -1,66 +1,33 @@
 package main
 
-import "fmt"
-
-type CheckingAccount struct {
-	owner         string
-	agency        int
-	accountNumber int
-	balance       float64
-}
-
-func (c *CheckingAccount) Withdraw(value float64) (string, bool) {
-	if value > 0 && value <= c.balance {
-		c.balance -= value
-		return "Successful withdrawal", true
-	}
-
-	return "Insufficient funds", false
-}
-
-func (c *CheckingAccount) Deposit(value float64) (string, float64) {
-	if value > 0 {
-		c.balance += value
-		return "Successful deposited", c.balance
-	}
-
-	return "The value should be higher than zero", c.balance
-}
-
-func (c *CheckingAccount) Transfer(value float64, destinyAccount *CheckingAccount) string {
-	_, withdrawed := c.Withdraw(value)
-
-	if withdrawed {
-		destinyAccount.Deposit(value)
-		return "Successful transfer"
-	}
-
-	return "Insufficient funds"
-}
+import (
+	"bank/accounts"
+	"fmt"
+)
 
 func main() {
-	myAccount := CheckingAccount{owner: "Wagner", agency: 123, accountNumber: 123456, balance: 1000.00}
-	otherAccount := CheckingAccount{owner: "Joao", balance: 100.00}
+	myAccount := accounts.CheckingAccount{Owner: "Wagner", Balance: 1000.00}
+	otherAccount := accounts.CheckingAccount{Owner: "Joao", Balance: 100.00}
 
 	fmt.Println(myAccount)
 
 	fmt.Println(myAccount.Withdraw(200))
-	fmt.Println(myAccount.balance)
+	fmt.Println(myAccount.Balance)
 
 	fmt.Println(myAccount.Withdraw(1200))
-	fmt.Println(myAccount.balance)
+	fmt.Println(myAccount.Balance)
 
 	fmt.Println(myAccount.Withdraw(-200))
-	fmt.Println(myAccount.balance)
+	fmt.Println(myAccount.Balance)
 
 	fmt.Println(myAccount.Deposit(-200))
-	fmt.Println(myAccount.balance)
+	fmt.Println(myAccount.Balance)
 
 	status, accountBalance := myAccount.Deposit(200)
 	fmt.Println("Status:", status)
 	fmt.Println("Account Balance:", accountBalance)
 
 	fmt.Println(myAccount.Transfer(200, &otherAccount))
-	fmt.Println(myAccount.balance)
-	fmt.Println(otherAccount.balance)
+	fmt.Println(myAccount.Balance)
+	fmt.Println(otherAccount.Balance)
 }
